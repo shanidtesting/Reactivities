815fbb8114b4a6ea2d171567fdc413f59b698c45
shanid <shanidtesting@gmail.com>
2021-08-10 07:38:54 +0530

    Activity interface.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
 
 function App() {
 
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/Activities').then(response => {
-      console.log(response);
+    axios.get<Activity[]>('http://localhost:5000/api/Activities').then(response => {
       setActivities(response.data);
     })
   }, []);
@@ -19,7 +17,7 @@ function App() {
     <div>
       <Header as='h2' icon='users' content='Reactivities' />
       <List>
-        {activities.map((activity: any) => (
+        {activities.map((activity) => (
           <List.Item key={activity.id}>
             {activity.title}
           </List.Item>
